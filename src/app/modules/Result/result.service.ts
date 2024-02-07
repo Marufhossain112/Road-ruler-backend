e1423924db195a556161099fba1b6d596b8786c6
Maruf Hossain <dev.maruf112@gmail.com>
2024-02-07 11:12:48 +0600

    update status based on score

--- src/app/modules/Result/result.service.ts
+++ src/app/modules/Result/result.service.ts
@@ -3,6 +3,7 @@ import ApiError from '../../../errors/ApiErrors'
 import { IResult } from './result.interface'
 import { Answer } from '../Answer/answer.model'
 import { Result } from './result.model'
+import { Question } from '../Question/question.model'
 
 const createResult = async (payload: IResult) => {
   const isUserAnswerExist = await Answer.find({
@@ -31,43 +32,29 @@ const createResult = async (payload: IResult) => {
     selected_answer: answer.selected_answer,
     correct_answer: answer.question_id.correct_answer,
   }))
-//   console.log('🚀 results', results)
+  //   console.log('🚀 results', results)
   // return
   for (const { selected_answer, correct_answer } of results) {
     if (selected_answer === correct_answer) {
       score++
     }
   }
+  const allQuestions = await Question.find()
+  const passingScore = 0.7
+  const totalQuestions = allQuestions.length
+//   console.log('🚀 total questions', totalQuestions)
+  const percentageScore = score / totalQuestions
+//   console.log('🚀 percentage Score', percentageScore)
+  const pass_status = percentageScore >= passingScore ? 'Passed' : 'Failed'
   result = await Result.findOneAndUpdate(
     { user_id: payload.user_id },
-    { score },
+    { score, pass_status },
     { new: true }
   )
   return result
 }
 
-// const getSingleAnswer = async (user_id: string, question_id: string) => {
-//   const result = await Answer.findOne({ user_id, question_id }).populate([
-//     {
-//       path: 'user_id',
-//     },
-//     {
-//       path: 'question_id',
-//     },
-//   ])
-//   return result
-// }
-// const getAllAnswersByUserId = async (user_id: string) => {
-//   const result = await Answer.find({ user_id }).populate([
-//     {
-//       path: 'user_id',
-//     },
-//     {
-//       path: 'question_id',
-//     },
-//   ])
-//   return result
-// }
+
 
 export const ResultService = {
   createResult,
